92a6a573e12b3ecca5dd91aa3eb570d9dc82fdc1
venkatasubramanyam <venkat.gsvs@gmail.com>
2022-03-26 11:54:38 +0530

    Update fileUploadConfig.js

--- api/fileupload/config/fileUploadConfig.js
+++ api/fileupload/config/fileUploadConfig.js
@@ -2,10 +2,10 @@
  * Configurations specific to FILE UPLOADING to S3
  */
 const uploadConfig = {
-     AWS_ACCESS_KEY : "AKIASQMB4AV7KKF2I5US",
-     SECRET_KEY : "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",
+     AWS_ACCESS_KEY : "",
+     SECRET_KEY : "",
      REGION : "us-east-2",
-     BUCKET_NAME : "socialmedia-app"
+     BUCKET_NAME : ""
 }
 
 export default uploadConfig
